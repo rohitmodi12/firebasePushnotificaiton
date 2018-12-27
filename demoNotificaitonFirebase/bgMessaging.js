966ca97eeedf7967d9c093a1de5eee6f8b6b3ca0
Rohit <rohit_macmini@Rohit-MacMinis-Mac-mini.local>
2018-12-27 14:33:38 +0530

    Firebase push notification in android with custom sound

---
+++ demoNotificaitonFirebase/bgMessaging.js
@@ -0,0 +1,10 @@
+// @flow
+import firebase from 'react-native-firebase';
+// Optional flow type
+import type { RemoteMessage } from 'react-native-firebase';
+
+export default async (message: RemoteMessage) => {
+    // handle your message
+
+    return Promise.resolve();
+}
